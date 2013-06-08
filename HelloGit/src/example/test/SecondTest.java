44f249d2ac993e01f12eba561deb09d81793a5a8
Surechun <tangxiaochun88@gmail.com>
2013-06-08 16:36:09 +0800

    Second commit

---
+++ HelloGit/src/example/test/SecondTest.java
@@ -0,0 +1,31 @@
+/*
+* 
+*
+* Copyright © 2013 Wisers Information Limited. 
+* Room 1408, Tower B, Phase 1, Tianan Hi-Tech Plaza, Tianan Cyber Park, 
+* Futian District, Shenzhen, Guangdong, China.
+* All rights reserved.
+*
+*
+*/
+package example.test;
+
+/**
+ * 
+ * 
+ * @version 1.0 2013-6-8
+ * @author jerrytang@wisers.com
+ */
+public class SecondTest
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
