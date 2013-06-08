50ffc84974382091408d552397244a7354a2fa38
Surechun <tangxiaochun88@gmail.com>
2013-06-08 17:30:15 +0800

    Second commit

--- HelloGit/src/example/test/HelloGit.java
+++ HelloGit/src/example/test/HelloGit.java
@@ -25,6 +25,8 @@ public class HelloGit
     public static void main(String[] args)
     {
         System.out.println("Hello Git.");
+        
+        System.out.println("Second commit.");
 
     }
 
