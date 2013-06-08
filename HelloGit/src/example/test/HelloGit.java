f4621c887c53a769ef8fd34957688042a75eb14a
Surechun <tangxiaochun88@gmail.com>
2013-06-08 16:25:53 +0800

    First commit.

---
+++ HelloGit/src/example/test/HelloGit.java
@@ -0,0 +1,31 @@
+/*
+* 
+*
+* Copyright © 2013 Wisers Information Limited. 
+* Room 1408, Tower B, Phase 1, Tianan Hi-Tech Plaza, Tianan Cyber Park, 
+* Futian District, Shenzhen, Guangdong, China.
+* All rights reserved.
+*
+*
+*/
+package example.test;
+
+/**
+ * 
+ * 
+ * @version 1.0 2013-6-8
+ * @author jerrytang@wisers.com
+ */
+public class HelloGit
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Git.");
+
+    }
+
+}
